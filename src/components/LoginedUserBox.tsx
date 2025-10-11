779c42d88c67c7e9a111ee86e78af99b13786399
lshy1993 <lshy1993@live.com>
2025-10-12 03:30:24 +0900

    add submodule; fix windows lf issue

--- src/components/LoginedUserBox.tsx
+++ src/components/LoginedUserBox.tsx
@@ -2,7 +2,7 @@ import { Avatar, Box, Button, Typography } from '@mui/material';
 
 export const LoginedUserBox = () => {
   // 假设有 user 状态
-  const user = useUser(); // 你可以用 context、redux、swr 等管理
+  const user = useUser();
   return (
     <Box sx={{ display: 'flex', alignItems: 'center', gap: 1, ml: 2 }}>
       {user ? (
